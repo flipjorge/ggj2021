74dabb674095aa9834cb20384aec7ffacd56ed0c
João Vieira <joao.vieira@miniclip.com>
2021-01-29 22:43:21 +0000

    Added simple Scriptable object reference implementation

---
+++ Assets/Scripts/References/CameraReference.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CameraReference", menuName = "Reference/CameraReference", order = 1)]
+public class CameraReference : Reference<Camera>
+{
+
+}
